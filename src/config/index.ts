2733db85b1a1b8fe2b2910e2091cbafafa778642
nobel7761 <habiburnobel@gmail.com>
2023-05-29 17:36:37 +0600

    setup cors, mongoose, express and parser

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import path from "path";
+
+//here we are joining the environment variable and this configure file. As this is not in the same directory. That is why we need to do this.
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+};
