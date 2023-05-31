ab9257ec8c4c5c57c4548132ca2456aa72d86039
nobel7761 <habiburnobel@gmail.com>
2023-06-01 03:42:23 +0600

    router, controller added and utlis file modified

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 
 const app: Application = express()
 
@@ -9,6 +10,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+//application routes
+app.use('/api/v1/users', usersRouter)
+
+//testing
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
