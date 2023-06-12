d0c96363ba5d2ecb7b48182a92b28eac91dff0f4
nobel7761 <habiburnobel@gmail.com>
2023-06-13 00:45:26 +0600

    zod installed

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,8 +1,24 @@
 import { RequestHandler } from 'express'
 import { UserService } from './user.service'
+import { z } from 'zod'
 
 const createUser: RequestHandler = async (req, res, next) => {
   try {
+    //req-validation
+    //body --> object
+    //data --> object
+
+    const createUSerZodSchema = z.object({
+      body: z.object({
+        role: z.string({
+          required_error: 'Role is required!!!',
+        }),
+        password: z.string().optional(),
+      }),
+    })
+
+    await createUSerZodSchema.parseAsync(req)
+
     const { user } = req.body
     const result = await UserService.createUser(user)
     res.status(200).json({
