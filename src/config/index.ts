a795db5248a89285677939a4c9440af4c68539a4
nobel7761 <habiburnobel@gmail.com>
2023-06-13 10:32:29 +0600

    normal test commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
-import dotenv from 'dotenv'
-import path from 'path'
+import dotenv from 'dotenv';
+import path from 'path';
 
 //here we are joining the environment variable and this configure file. As this is not in the same directory. That is why we need to do this.
-dotenv.config({ path: path.join(process.cwd(), '.env') })
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
   default_user_pass: process.env.DEFAULT_USER_PASS,
-}
+};
--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,46 @@
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config/index'
-import { errorlogger, logger } from './shared/logger'
-import { Server } from 'http'
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+import { errorlogger, logger } from './shared/logger';
+import { Server } from 'http';
 
 process.on('uncaughtException', error => {
-  errorlogger.error(error)
-  process.exit(1)
-})
+  errorlogger.error(error);
+  process.exit(1);
+});
 
-let server: Server
+let server: Server;
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string)
-    logger.info(`🛢   Database is connected successfully`)
+    await mongoose.connect(config.database_url as string);
+    logger.info(`🛢   Database is connected successfully`);
 
     server = app.listen(config.port, () => {
-      logger.info(`Application  listening on port ${config.port}`)
-    })
+      logger.info(`Application  listening on port ${config.port}`);
+    });
   } catch (err) {
-    errorlogger.error('Failed to connect database', err)
+    errorlogger.error('Failed to connect database', err);
   }
 
   process.on('unhandledRejection', error => {
     if (server) {
       server.close(() => {
-        errorlogger.error(error)
-        process.exit(1)
-      })
+        errorlogger.error(error);
+        process.exit(1);
+      });
     } else {
-      process.exit(1)
+      process.exit(1);
     }
-  })
+  });
 }
 
-main()
+main();
 
 //!this SIGTERM means signal termination. sometimes server may close for any reason. On the closing time we want a signal when the server was closed. this is why we are using it.
 process.on('SIGTERM', () => {
-  logger.info('SIGTERM  is received')
+  logger.info('SIGTERM  is received');
   if (server) {
-    server.close()
+    server.close();
   }
-})
+});
