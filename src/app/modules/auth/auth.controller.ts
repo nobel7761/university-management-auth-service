9759b30cc21d94b89b2f248fcbeaeafd1ce20e34
nobel7761 <habiburnobel@gmail.com>
2023-07-17 10:57:54 +0600

    set refresh token into cookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,16 +4,26 @@ import sendResponse from '../../../shared/sendResponse';
 import httpStatus from 'http-status';
 import { AuthService } from './auth.service';
 import { ILoginUserResponse } from './auth.interface';
+import config from '../../../config';
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const { ...loginData } = req.body;
   const result = await AuthService.loginUser(loginData);
 
+  const { refreshToken, ...others } = result as ILoginUserResponse;
+
+  // set refresh token into cookie
+  const cookieOptions = {
+    secure: config.env === 'production' ? true : false,
+    httpOnly: true, // to make sure that this cookie won't be accessible from client side
+  };
+  res.cookie('refreshToken', refreshToken, cookieOptions);
+
   sendResponse<ILoginUserResponse>(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'User Logged In Successfully!',
-    data: result,
+    data: others,
   });
 });
 
